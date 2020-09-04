333039c57511e6e59b7d62b8a193d26474c9f9cb
Zhijian Dong <zhijian.dong@accenture.com>
2020-09-04 15:01:39 +0800

    Second Commit.

--- demorest/src/main/java/com/acn/demorest/AlienRepository.java
+++ demorest/src/main/java/com/acn/demorest/AlienRepository.java
@@ -2,45 +2,85 @@ package com.acn.demorest;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.sql.*;
 
 public class AlienRepository {
-	
-	List<Alien> aliens;
-	
+
+	Connection con = null;
+
 	public AlienRepository() {
-		aliens = new ArrayList<>();
-		
-		Alien a1 = new Alien();
-		a1.setId(101);
-		a1.setName("A1");
-		a1.setPoints(80);
-		
-		Alien a2 = new Alien();
-		a2.setId(102);
-		a2.setName("A2");
-		a2.setPoints(50);
-		
-		aliens.add(a1);
-		aliens.add(a2);
+		String url = "jdbc:mysql://localhost:3306/restdb";
+		String user = "root";
+		String password = "root";
+		try {
+//			Class.forName("com.mysql.jdbc.Driver");
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con = DriverManager.getConnection(url, user, password);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
 	}
 
 	public List<Alien> getAliens() {
+		List<Alien> aliens = new ArrayList<>();
+		String sql = "select * from alien";
+		try {
+			Statement st = con.createStatement();
+			ResultSet rs = st.executeQuery(sql);
+			while(rs.next()) {
+				Alien a = new Alien();
+				a.setId(rs.getInt(1));
+				a.setName(rs.getString(2));
+				a.setPoints(rs.getInt(3));
+				aliens.add(a);
+			}
+		}catch(Exception e) {
+			System.out.println(e);
+		}
+		
 		return aliens;
 	}
 
 	public Alien getAlien(int id) {
-		for (Alien a : aliens) {
-			if (a.getId() == id) {
-				return a;
+		String sql = "select * from alien where id = "+id;
+		Alien a = new Alien();
+		try {
+			Statement st = con.createStatement();
+			ResultSet rs = st.executeQuery(sql);
+			if(rs.next()) {
+				a.setId(rs.getInt(1));
+				a.setName(rs.getString(2));
+				a.setPoints(rs.getInt(3));
 			}
+		}catch(Exception e) {
+			System.out.println(e);
 		}
-		return new Alien();
+		return a;		
 	}
 
-	public Alien create(Alien a1) {
-		System.out.print(a1);
-		aliens.add(a1);
-		return a1;
+	public void create(Alien a1) {
+		String sql = "insert into alien values (?, ?, ?)";
+		try {
+			PreparedStatement st = con.prepareStatement(sql);
+			st.setInt(1, a1.getId());
+			st.setString(2, a1.getName());
+			st.setInt(3, a1.getPoints());
+			st.executeUpdate();
+		}catch(Exception e) {
+			System.out.println(e);
+		}
 	}
 	
+	public void update(Alien a1) {
+		String sql = "update alien set name=?, points=? where id=?";
+		try {
+			PreparedStatement st = con.prepareStatement(sql);
+			st.setString(1, a1.getName());
+			st.setInt(2, a1.getPoints());
+			st.setInt(3, a1.getId());
+			st.executeUpdate();
+		}catch(Exception e) {
+			System.out.println(e);
+		}
+	}
 }
--- demorest/src/main/java/com/acn/demorest/AlienResource.java
+++ demorest/src/main/java/com/acn/demorest/AlienResource.java
@@ -2,8 +2,10 @@ package com.acn.demorest;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -23,15 +25,24 @@ public class AlienResource {
 	
 	@GET
 	@Path("alien/{id}")
-	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML}) // Out Type
 	public Alien getAlien(@PathParam("id") int id) {		
 		return repo.getAlien(id);
 	}
 	
 	@POST
 	@Path("alien")
+	@Consumes({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML}) // In Type
 	public Alien createAlien(Alien a1) {
 		repo.create(a1);
+		return a1;
+	}
+	
+	@PUT
+	@Path("alien")
+	@Consumes({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML}) // In Type
+	public Alien updateAlien(Alien a1) {
+		repo.update(a1);
 		return a1;
 	}
 
